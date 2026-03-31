95a54b3070d1ac856e00c8539a8058421fcbc4ab
김종관/선임/데이터메쉬(주) <BP32391@bp.hd.com>
2026-03-31 10:23:24 +0900

    [Edit] models struct

--- models/name.py
+++ models/name.py
@@ -1,9 +1,13 @@
+import uuid
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy_utils import UUIDType
 
 from database import Base
+
+from .utils import created_at, updated_at
 
 if TYPE_CHECKING:
     from .record import Record
@@ -12,9 +16,12 @@ if TYPE_CHECKING:
 class Name(Base):
     __tablename__ = "names"
 
-    id: Mapped[str] = mapped_column(String(255), primary_key=True)
+    id: Mapped[UUIDType] = mapped_column(UUIDType, default=uuid.uuid4, primary_key=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False, comment="이름 항목")
-    count: Mapped[str] = mapped_column(String(255), nullable=False, comment="건수")
+    count: Mapped[int] = mapped_column(Integer, nullable=False, comment="건수")
+
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
 
     # 연결관계
     records: Mapped[list["Record"]] = relationship(
--- models/record.py
+++ models/record.py
@@ -1,6 +1,10 @@
 # models/stat.py
-from sqlalchemy import ForeignKey, String
+import uuid
+from datetime import date
+
+from sqlalchemy import Date, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy_utils import UUIDType
 
 from database import Base
 from models.name import Name
@@ -9,23 +13,22 @@ from .utils import created_at, updated_at
 
 
 class Record(Base):
-    __tablename__ = "record"
+    __tablename__ = "records"
 
-    id: Mapped[str] = mapped_column(String(255), primary_key=True)
-    name_id: Mapped[str] = mapped_column(
-        String(255), ForeignKey("names.id"), nullable=False, comment="이름 FK"
+    id: Mapped[UUIDType] = mapped_column(UUIDType, default=uuid.uuid4, primary_key=True)
+    name_id: Mapped[UUIDType] = mapped_column(
+        UUIDType, ForeignKey("names.id"), nullable=False, comment="이름 FK"
     )
     city: Mapped[str] = mapped_column(String(255), nullable=False, comment="도시")
-    record_date: Mapped[str] = mapped_column(
-        String(255), nullable=False, comment="기록일자"
-    )
+    record_date: Mapped[date] = mapped_column(Date, nullable=False, comment="기록일자")
+
     gender: Mapped[str] = mapped_column(String(255), nullable=False, comment="성별")
-    count: Mapped[str] = mapped_column(
-        String(255), nullable=False, comment="성별 전체 건수"
+    count: Mapped[int] = mapped_column(
+        Integer, nullable=False, comment="성별 전체 건수"
     )
 
     created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
 
     # 연결관계
-    name: Mapped["Name"] = relationship("Name", back_populates="record")
+    name: Mapped["Name"] = relationship("Name", back_populates="records")
